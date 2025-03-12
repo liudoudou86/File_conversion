91d3677ceda30ad581c6679d02e88651e6437951
liudoudou86 <543972930@qq.com>
2025-03-12 10:03:38 +0800

    feat：优化当前逻辑

--- FileConversion.py
+++ FileConversion.py
@@ -9,90 +9,114 @@ from pdf2docx import Converter
 from docx2pdf import convert
 from xmind2testcase.zentao import xmind_to_zentao_csv_file
 
-
 # 获取当前时间
-time_tup=time.localtime(time.time())
-format_time="%Y-%m-%d_%H%M%S"
-cur_time=time.strftime(format_time,time_tup)
+def get_current_time():
+    time_tup = time.localtime(time.time())
+    format_time = "%Y-%m-%d_%H%M%S"
+    return time.strftime(format_time, time_tup)
 
-def pdf_to_word():
+# 确保输出目录存在
+def ensure_output_dir(output_dir=None):
+    if output_dir is None:
+        # 使用用户文档目录作为默认位置
+        user_docs = os.path.join(os.path.expanduser('~'), 'Documents')
+        output_dir = os.path.join(user_docs, 'FileOutput')
+    
+    try:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        return output_dir
+    except Exception as e:
+        # 如果创建目录失败，回退到当前目录
+        fallback_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output')
+        if not os.path.exists(fallback_dir):
+            os.makedirs(fallback_dir)
+        sg.popup_error(f'无法创建指定目录: {str(e)}\n已使用备用目录: {fallback_dir}', font='微软雅黑')
+        return fallback_dir
 
-    # 存储路径识别
-    isExists=os.path.exists('D:\@@@')
-    if not isExists:
-        os.mkdir('D:\@@@')
-    else:
-        pass
+def pdf_to_word(address):
+    try:
+        output_dir = ensure_output_dir()
+        cur_time = get_current_time()
+        wordfile_path = os.path.join(output_dir, f'wordfile_{cur_time}.docx')
 
-    pdf_file = address
-    wordfile_path = 'D://@@@//wordfile_{}.docx'.format(cur_time)
+        cv = Converter(address)
+        cv.convert(wordfile_path, start=0, end=None)
+        cv.close()
+        return True
+    except Exception as e:
+        sg.popup_error(f'PDF转Word失败: {str(e)}', font='微软雅黑')
+        return False
 
-    cv = Converter(pdf_file)
-    cv.convert(wordfile_path, start=0, end=None)
-    cv.close()
+def word_to_pdf(address):
+    try:
+        output_dir = ensure_output_dir()
+        cur_time = get_current_time()
+        pdffile_path = os.path.join(output_dir, f'pdffile_{cur_time}.pdf')
 
-def word_to_pdf():
+        convert(address, pdffile_path)
+        return True
+    except Exception as e:
+        sg.popup_error(f'Word转PDF失败: {str(e)}', font='微软雅黑')
+        return False
 
-    # 存储路径识别
-    isExists=os.path.exists('D:\@@@')
-    if not isExists:
-        os.mkdir('D:\@@@')
-    else:
-        pass
+def xmind_conversion(address):
+    try:
+        output_dir = ensure_output_dir()
+        cur_time = get_current_time()
 
-    word_file = address
-    pdffile_path = 'D://@@@//pdffile_{}.pdf'.format(cur_time)
+        old_csvfile = xmind_to_zentao_csv_file(address)
+        old_csvfile_path = os.path.split(old_csvfile)
+        new_csvfile = os.path.join(old_csvfile_path[0], f'csvfile_{cur_time}.csv')
+        os.rename(old_csvfile, new_csvfile)
+        shutil.move(new_csvfile, output_dir)
+        return True
+    except Exception as e:
+        sg.popup_error(f'Xmind转CSV失败: {str(e)}', font='微软雅黑')
+        return False
 
-    convert(word_file, pdffile_path)
+def main():
+    layout = [
+        [sg.Radio('PDF转word', 'RADIO1', key='_RADIO11_', default=True), 
+         sg.Radio('Word转pdf', 'RADIO1', key='_RADIO12_')],
+        [sg.Radio('Xmind转csv', 'RADIO1', key='_RADIO13_')],
+        [sg.Input(key='_ADDRESS_', font='微软雅黑', size=(20, 1)), 
+         sg.FileBrowse('选择文件', font='微软雅黑', size=(8, 1))],
+        [sg.Button('文件转换', key='_CONFIRM_', font='微软雅黑', size=(8, 1)),
+         sg.Button('打开目录', key='_FOLDER_', size=(8, 1)), 
+         sg.Exit('退出', key='_EXIT_', font='微软雅黑', size=(8, 1))]
+    ]
+    
+    # 定义窗口，窗口名称
+    window = sg.Window('文件转换工具', layout, font='微软雅黑')
+    
+    # 自定义窗口进行数值回显
+    while True:
+        event, values = window.read()
+        
+        if event == '_CONFIRM_':
+            address = values.get('_ADDRESS_', '')
+            
+            if not address:
+                sg.popup_error('请选择文件', font='微软雅黑')
+                continue
+                
+            if values.get('_RADIO11_'):
+                if pdf_to_word(address):
+                    sg.popup('转换已完成', font='微软雅黑')
+            elif values.get('_RADIO12_'):
+                if word_to_pdf(address):
+                    sg.popup('转换已完成', font='微软雅黑')
+            elif values.get('_RADIO13_'):
+                if xmind_conversion(address):
+                    sg.popup('转换已完成', font='微软雅黑')
+        
+        elif event == '_FOLDER_':
+            output_dir = ensure_output_dir()
+            os.startfile(output_dir)
+        
+        elif event in ['_EXIT_', None]:
+            break
 
-def xmind_conversion():
-
-    # 存储路径识别
-    isExists=os.path.exists('D:\@@@')
-    if not isExists:
-        os.mkdir('D:\@@@')
-    else:
-        pass
-
-    xmind_file = address
-    csvfile_path = 'D://@@@'
-
-    old_csvfile = xmind_to_zentao_csv_file(xmind_file)
-    old_csvfile_path = os.path.split(old_csvfile)
-    new_csvfile = old_csvfile_path[0] + '\csvfile_{}.csv'.format(cur_time)
-    os.rename(old_csvfile, new_csvfile)
-    shutil.move(new_csvfile, csvfile_path)
-
-
-layout = [
-    [sg.Radio('PDF转word', 'RADIO1', key='_RADIO11_', default=True), sg.Radio('Word转pdf', 'RADIO1', key='_RADIO12_')],
-    [sg.Radio('Xmind转csv', 'RADIO1', key='_RADIO13_')],
-    [sg.Input(key = '_ADDRESS_', font='微软雅黑', size=(20, 1)), sg.FileBrowse('选择文件', font='微软雅黑', size=(8, 1))],
-    [sg.Button('文件转换', key = '_CONFIRM_', font='微软雅黑', size=(8, 1)), sg.Button('打开', key='_FOLDER_', size=(8, 1)), sg.Exit('退出', key = '_EXIT_', font='微软雅黑', size=(8, 1))]
-]
-# 定义窗口，窗口名称
-window = sg.Window('文件转换工具',layout,font='微软雅黑')
-# 自定义窗口进行数值回显
-while True:
-    event,values = window.read()
-    if event == '_CONFIRM_':
-        if values.get('_RADIO11_','True'):
-            address = values['_ADDRESS_']
-            pdf_to_word = pdf_to_word()
-            sg.popup('转换已完成', font='微软雅黑')
-        elif values.get('_RADIO12_','True'):
-            address = values['_ADDRESS_']
-            word_to_pdf = word_to_pdf()
-            sg.popup('转换已完成', font='微软雅黑')
-        elif values.get('_RADIO13_','True'):
-            address = values['_ADDRESS_']
-            xmind_conversion = xmind_conversion()
-            sg.popup('转换已完成', font='微软雅黑')
-        else:
-            pass
-    elif event == '_FOLDER_':
-        os.startfile(r"D:\\@@@")
-    elif event in ['_EXIT_',None]:
-        break
-    else:
-        pass
+if __name__ == "__main__":
+    main()
